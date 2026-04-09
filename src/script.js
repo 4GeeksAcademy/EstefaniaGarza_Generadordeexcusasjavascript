f393941f543c40b9f2083858cbecd75fb13d8e8a
Estefania Garza <lorenagh910@hotmail.com>
2026-04-09 04:19:21 +0000

    First commit

--- src/script.js
+++ src/script.js
@@ -1,10 +1,3 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
 let who = ['The dog ', 'My grandma ', 'The mailman ', 'My bird '];
 let action = ['ate ', 'peed ', 'crushed ', 'broke '];
